0036c53f02499c36315e102db9929f71bfef5544
Timothée VAUCHY <timvauchy@gmail.com>
2023-03-24 14:15:15 +0100

    NACA Constructor modification

--- BladeGenerator/loc_utils/NACA.py
+++ BladeGenerator/loc_utils/NACA.py
@@ -1,8 +1,14 @@
+from typing import Union
+
 class NACA4:
-    def __init__(self, NACA_str: str):
-        self.m = int(NACA_str[0])
-        self.p = int(NACA_str[1])
-        self.t = int(NACA_str[2:])
+    def __init__(self, NACA_code: Union[str, int]):
+        if type(NACA_code) == int:
+            NACA_code = str(NACA_code)
+        elif type(NACA_code) != str:
+            raise TypeError("NACA code must be a string or an integer")
+        self.m = int(NACA_code[0])
+        self.p = int(NACA_code[1])
+        self.t = int(NACA_code[2:])
 
 # class NACA5:
 #     def __init__(self, NACA_str: str):
